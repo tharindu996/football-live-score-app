b7ea8f89ad0ae0f1e9b055ce1be451ab45aabbdb
Tharindu <tharindudissanayake03@gmail.com>
2025-07-30 13:41:59 +0530

    create index route

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class BaseController extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Events\ScoreUpdated;
+use App\Http\Controllers\BaseController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BaseController::class, 'index']);
 
 
 Route::post('/goal/{team}', function ($team) {
