2167fd530123a157b97fa4f7649292a76fce5441
Tharindu <tharindudissanayake03@gmail.com>
2025-07-30 16:07:09 +0530

    add unique to team title

--- database/factories/TeamFactory.php
+++ database/factories/TeamFactory.php
@@ -17,7 +17,7 @@ class TeamFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => $this->faker->word(),
+            'title' => $this->faker->unique()->word(),
         ];
     }
 }
