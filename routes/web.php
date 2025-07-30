b0bf557d75e8d1ff998de30459ea5714c685d920
Tharindu <tharindudissanayake03@gmail.com>
2025-07-30 16:07:31 +0530

    add route functions

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Events\ScoreUpdated;
 use App\Http\Controllers\BaseController;
 use App\Http\Controllers\FootballMatchController;
+use App\Http\Controllers\FootballMatchTeamController;
 use App\Http\Controllers\MatchController;
+use App\Http\Controllers\ScoreboardController;
 use App\Http\Controllers\TeamController;
 
 Route::get('/', [BaseController::class, 'index']);
 Route::resource('teams', TeamController::class)->except(['create', 'show']);
+Route::patch('/football-matches/{football_match}/update-score/{team}', [FootballMatchController::class, 'updateScore'])->name('football-match.update-score');
+Route::patch('/football-matches/{football_match}/update-status', [FootballMatchController::class, 'updateStatus'])->name('football-match.update-status');
 Route::resource('football-matches', FootballMatchController::class)->only(['index', 'store', 'destroy']);
+Route::get('/scoreboard', [ScoreboardController::class, 'index'])->name('scoreboard.index');
+
+//Route::resource('football-matches.teams', FootballMatchTeamController::class)->only(['update']);
 
 // Route::post('/goal/{team}', function ($team) {
 //     // Simulate score increment (use session or DB in real app)
@@ -25,14 +32,12 @@ Route::resource('football-matches', FootballMatchController::class)->only(['inde
 
 //     ScoreUpdated::dispatch($teamA, $teamB);
 //     return response()->json(['message' => 'Goal recorded!']);
-// })->name('goal');
+// })->name('goal'); //done
 
+// Route::get('/live-score', function () {
+//     return view('live-score');
+// }); //done
 
-
-Route::get('/live-score', function () {
-    return view('live-score');
-});
-
-Route::get('/score', function () {
-    return view('score');
-});
+// Route::get('/score', function () {
+//     return view('score');
+// }); //done
