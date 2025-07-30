3f5e47b1ab30b12f23baf34253c7ca0aca5e69ea
Tharindu <tharindudissanayake03@gmail.com>
2025-07-30 17:11:06 +0530

    update layout

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends("components.layouts.base")
 
-<head>
-    <meta charset="UTF-8">
-    <title>Live Score</title>
-    @vite(["resources/css/app.css", "resources/js/app.js"])
-</head>
-<body>
-    <h1>Live Football Score</h1>
-    <div id="connection-status"></div>
-    <div>
-        <h2>Live Score</h2>
-        <p id="score">Team A 0 - 0 Team B</p>
-    </div>    
-</body>
-</html>
+@section("content")
+    @if (empty($ongoingFootballMatch))
+        <h1>Live Football Score</h1>
+        <div id="connection-status"></div>
+        <div>
+            <h2>Live Score</h2>
+            <p id="score">Team A 0 - 0 Team B</p>
+        </div>
+    @else
+        <div class="">
+            No ongoing match is found.
+        </div>
+    @endif
+@endsection
