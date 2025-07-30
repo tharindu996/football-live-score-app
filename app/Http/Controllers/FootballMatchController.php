20e41d2bb8a74714e79ef92f6a753eaded28fd8e
Tharindu <tharindudissanayake03@gmail.com>
2025-07-30 16:05:55 +0530

    create football match route functions

--- app/Http/Controllers/FootballMatchController.php
+++ app/Http/Controllers/FootballMatchController.php
@@ -3,12 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Enums\FootballMatchStatus;
+use App\Events\ScoreUpdated;
 use App\Models\FootballMatch;
 use App\Http\Requests\StoreFootballMatchRequest;
 use App\Http\Requests\UpdateFootballMatchRequest;
+use App\Http\Requests\UpdateFootballMatchStatusRequest;
+use App\Models\Team;
 
 class FootballMatchController extends Controller
 {
+    /**
+     * update match status.
+     */
+    public function updateStatus(UpdateFootballMatchStatusRequest $request, FootballMatch $footballMatch)
+    {
+        $inputs = $request->validated();
+        $footballMatch->update(['status' => $inputs['status'],]);
+    }
+
+
+    /**
+     * update match score.
+     */
+    public function updateScore(FootballMatch $footballMatch, Team $team)
+    {
+        $teamA = session('teamA', $footballMatch->home_score);
+        $teamB = session('teamB', $footballMatch->away_score);
+
+        if ($team->id === $footballMatch->homeTeam->id) {
+            $teamA++;
+            $footballMatch->update(['home_score' => $teamA]);
+        } else {
+            $teamB++;
+            $footballMatch->update(['away_score' => $teamA]);
+        }
+        session(['teamA' => $teamA, 'teamB' => $teamB]);
+
+        ScoreUpdated::dispatch($teamA, $teamB);
+        return redirect()->back()->with(['success' => 'Score updated successfully']);
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -24,7 +58,7 @@ class FootballMatchController extends Controller
     {
         $inputs = $request->validated();
         $ongoingMatchCount = FootballMatch::where('status', FootballMatchStatus::ONGOING)->count();
-        if($ongoingMatchCount > 0){
+        if ($ongoingMatchCount > 0) {
             return redirect()->back()->with(['errors' => 'Can not create a match when there is a ongoing match.']);
         }
         FootballMatch::create($inputs);
