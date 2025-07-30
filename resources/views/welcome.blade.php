0dddfeb4ddb8ee865917f928b2917a9cb5600ea0
Tharindu <tharindudissanayake03@gmail.com>
2025-07-30 13:35:34 +0530

    clean code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,20 +6,12 @@
     <title>Live Score</title>
     @vite(["resources/css/app.css", "resources/js/app.js"])
 </head>
-
 <body>
-
     <h1>Live Football Score</h1>
     <div id="connection-status"></div>
     <div>
         <h2>Live Score</h2>
         <p id="score">Team A 0 - 0 Team B</p>
-    </div>
-
-    <script>
-       
-        
-    </script>
+    </div>    
 </body>
-
 </html>
