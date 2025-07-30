d48039894b0e76571687b69f092b1a67424c69c9
Tharindu <tharindudissanayake03@gmail.com>
2025-07-30 18:24:46 +0530

    update index page

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -1,16 +1,20 @@
 @extends("components.layouts.base")
 
 @section("content")
-    @if (empty($ongoingFootballMatch))
-        <h1>Live Football Score</h1>
-        <div id="connection-status"></div>
-        <div>
-            <h2>Live Score</h2>
-            <p id="score">Team A 0 - 0 Team B</p>
-        </div>
-    @else
+    <div class="grid-container">
         <div class="">
-            No ongoing match is found.
+            @if ($ongoingFootballMatch == 1)
+                <h1>Live Football Score</h1>
+                <div id="connection-status"></div>
+                <div>
+                    <h2>Live Score</h2>
+                    <p id="score">Team A 0 - 0 Team B</p>
+                </div>
+            @else
+                <div class="">
+                    No ongoing match is found.
+                </div>
+            @endif
         </div>
-    @endif
+    </div>
 @endsection
