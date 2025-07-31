f15d92cbab6902eb99466c3eedaf1d98f2a85255
Tharindu <tharindudissanayake03@gmail.com>
2025-07-31 15:32:11 +0530

    update footer

--- resources/views/components/includes/footer.blade.php
+++ resources/views/components/includes/footer.blade.php
@@ -1,74 +1,17 @@
-
 <footer class="bg-dark text-white pt-5 pb-4 mt-auto">
-        <div class="container text-center text-md-start">
-            <div class="row">
-                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Your Company</h5>
-                    <p>We are a company committed to delivering high-quality solutions for your needs. Innovation is our drive.</p>
-                </div>
-
-                <hr class="w-100 clearfix d-md-none" />
-
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold">Products</h5>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Product A</a></p>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Product B</a></p>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Product C</a></p>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Product D</a></p>
-                </div>
-
-                <hr class="w-100 clearfix d-md-none" />
-
-                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold">Useful Links</h5>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Your Account</a></p>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Become an Affiliate</a></p>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Shipping Rates</a></p>
-                    <p><a href="#" class="text-white" style="text-decoration: none;">Help</a></p>
-                </div>
-
-                <hr class="w-100 clearfix d-md-none" />
-
-                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                    <h5 class="text-uppercase mb-4 font-weight-bold">Contact</h5>
-                    <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                    <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-                    <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                    <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-                </div>
+    <div class="container text-center text-md-start">
+        <div class="row align-items-center">
+            <div class="col-12 text-center">
+                <p class="mb-0">
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script> All rights reserved by:
+                    <a href="#" style="text-decoration: none;"><strong
+                            class="text-warning">YourCompany.com</strong></a>
+                </p>
             </div>
 
-            <hr class="mb-4" />
-
-            <div class="row align-items-center">
-                <div class="col-md-7 col-lg-8">
-                    <p class="mb-0">
-                        Copyright &copy; <script>document.write(new Date().getFullYear())</script> All rights reserved by:
-                        <a href="#" style="text-decoration: none;"><strong class="text-warning">YourCompany.com</strong></a>
-                    </p>
-                </div>
-
-                <div class="col-md-5 col-lg-4">
-                    <div class="text-center text-md-end">
-                        <ul class="list-unstyled list-inline">
-                            <li class="list-inline-item">
-                                <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-facebook"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-twitter"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-google-plus"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-linkedin-in"></i></a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-youtube"></i></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
         </div>
-    </footer>
+    </div>
+</footer>
