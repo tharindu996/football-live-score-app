2986833a4765359bb51c8fef02addaa4d8403bd9
Tharindu <tharindudissanayake03@gmail.com>
2025-08-01 12:00:54 +0530

    add route name

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,5 @@ Route::resource('teams', TeamController::class)->only(['index', 'store', 'destro
 Route::patch('/football-matches/{football_match}/update-score/{team}', [FootballMatchController::class, 'updateScore'])->name('football-match.update-score');
 Route::patch('/football-matches/{football_match}/update-status', [FootballMatchController::class, 'updateStatus'])->name('football-match.update-status');
 Route::resource('football-matches', FootballMatchController::class)->only(['index', 'store', 'destroy']);
-Route::get('/scoreboard', [ScoreboardController::class, 'index']);
+Route::get('/scoreboard', [ScoreboardController::class, 'index'])->name('scoreboard.index');
 
